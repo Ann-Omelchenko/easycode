4f9b07afee0bd0d676c3c30ac4570c6f1850ce9c
Ann-Omelchenko <ann_omelchenko@meta.ua>
2016-05-23 20:53:53 +0300

    1

---
+++ 23.05/form.php
@@ -0,0 +1,45 @@
+<form class="form-horizontal" method="GET" action=" ">
+    <meta charset="utf-8">
+    <center>
+        <div class="form-group">
+            <label for="inputName3" class="col-sm-2 control-label">Product name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputName3" placeholder="Product name"><p></p>
+            </div>
+        </div>
+        <div>
+            <div>Product category</div>
+            <select class="form-control">
+                <option>Электроника</option>
+                <option>Одежда</option>
+                <option>Мебель</option>
+            </select>
+            <p></p>
+        </div>
+        <div class="form-group">
+            <label for="inputCost3" class="col-sm-2 control-label">Product cost</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputCost3" placeholder="Product cost"><p></p>
+            </div>
+        </div>
+
+
+
+        
+        <!--
+        <div>
+            <input type="submit" value="войти!">
+        </div>
+    </center>
+</form>
+<!-- Имя -->
+<!-- Фамилия -->
+<!-- Возраст -->
+<!-- age -->
+<!-- Пол. Radio checkbox -->
+<!-- Select. Город -->
+<!-- Дополнительное задание -->
+<!--<select multiple="multiple" name="somename[]">
+    <option>1</option>
+    <option>2</option>
+</select>-->
